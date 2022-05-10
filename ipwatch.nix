c931bd021d1e92dc87fb4924a239a090974901d5
Jared Baur <jaredbaur@fastmail.com>
2022-05-10 13:50:34 -0700

    Update vendorSha256

--- ipwatch.nix
+++ ipwatch.nix
@@ -3,5 +3,5 @@ buildGo118Module {
   pname = "ipwatch";
   version = "0.0.1";
   src = builtins.path { path = ./.; };
-  vendorSha256 = "sha256-I5RnpjSYFGfWu4PU6P6GYbuMI/A2/QjjXzVjsY3/4a8=";
+  vendorSha256 = "sha256-8lgxk2eO60USN6i8v+/2pKHlYnNtoyuCoOrKhbtMpb0=";
 }
