cd2f3e73adcd39a3205c943cbeb54a92642b24d8
Jared Baur <jaredbaur@fastmail.com>
2022-05-17 09:57:09 -0700

    Call continue on correct loop

--- main.go
+++ main.go
@@ -30,6 +30,7 @@ func main() {
 
 	c, err := netlink.Dial(unix.NETLINK_ROUTE, &netlink.Config{
 		Groups: unix.RTMGRP_IPV4_IFADDR,
+		Strict: true,
 	})
 	if err != nil {
 		log.Fatalf("failed to dial netlink: %v", err)
@@ -45,60 +46,55 @@ func main() {
 	for {
 		msgs, err := c.Receive()
 		if err != nil {
-			log.Println(err)
+			log.Printf("Failed to receive messages: %v", err)
 			break
 		}
+	messages:
 		for _, msg := range msgs {
-			if msg.Header.Type == unix.NLMSG_DONE {
+			if msg.Header.Type != unix.RTM_NEWADDR {
 				continue
 			}
-			if msg.Header.Type == unix.NLMSG_ERROR {
-				// TODO(jared): parse error
-				log.Println("NLMSG_ERROR", msg.Data)
+
+			var ifaceName, newIP string
+
+			decoder, err := netlink.NewAttributeDecoder(msg.Data[unix.SizeofIfAddrmsg:])
+			if err != nil {
+				log.Printf("Could not get attribute decoder: %v", err)
 				continue
 			}
-			if msg.Header.Type == unix.RTM_NEWADDR {
-				var ifaceName, newIP string
 
-				decoder, err := netlink.NewAttributeDecoder(msg.Data[unix.SizeofIfAddrmsg:])
-				if err != nil {
-					log.Println(err)
-					continue
-				}
-
-				for decoder.Next() {
-					switch decoder.Type() {
-					case unix.IFA_ADDRESS:
-						ip := decoder.Bytes()
-						if len(ip) != 4 {
-							log.Println("Did not get correct number of bytes")
-							continue
-						}
-						newIP = net.IPv4(ip[0], ip[1], ip[2], ip[3]).String()
-					case unix.IFA_LABEL:
-						ifaceName = decoder.String()
-						interested := false
-						for _, iface := range ifacesOfInterest {
-							interested = ifaceName == iface
-						}
-						if !interested {
-							continue
-						}
+			for decoder.Next() {
+				switch decoder.Type() {
+				case unix.IFA_ADDRESS:
+					ip := decoder.Bytes()
+					if len(ip) != 4 {
+						log.Println("Did not get correct number of bytes")
+						continue
+					}
+					newIP = net.IPv4(ip[0], ip[1], ip[2], ip[3]).String()
+				case unix.IFA_LABEL:
+					ifaceName = decoder.String()
+					interested := false
+					for _, iface := range ifacesOfInterest {
+						interested = ifaceName == iface
+					}
+					if !interested {
+						continue messages
 					}
 				}
-
-				cmd := exec.Command(filepath.Join(*exeToRun))
-				cmd.Stdout = os.Stdout
-				cmd.Stderr = os.Stderr
-				cmd.Env = append(cmd.Env, os.Environ()...)
-				cmd.Env = append(cmd.Env, fmt.Sprintf("IFACE=%s", ifaceName))
-				cmd.Env = append(cmd.Env, fmt.Sprintf("ADDR=%s", newIP))
-				log.Println("================================================================================")
-				if err := cmd.Run(); err != nil {
-					log.Printf("Error running exe: %v", err)
-				}
-				log.Println("================================================================================")
 			}
+
+			cmd := exec.Command(filepath.Join(*exeToRun))
+			cmd.Stdout = os.Stdout
+			cmd.Stderr = os.Stderr
+			cmd.Env = append(cmd.Env, os.Environ()...)
+			cmd.Env = append(cmd.Env, fmt.Sprintf("IFACE=%s", ifaceName))
+			cmd.Env = append(cmd.Env, fmt.Sprintf("ADDR=%s", newIP))
+			log.Println("================================================================================")
+			if err := cmd.Run(); err != nil {
+				log.Printf("Error running exe: %v", err)
+			}
+			log.Println("================================================================================")
 		}
 	}
 }
