acf895e5a17b53120ac5f7c40314827808150305
Jared Baur <jaredbaur@fastmail.com>
2022-10-21 19:11:04 -0700

    Add staticcheck

--- default.nix
+++ default.nix
@@ -1,8 +1,9 @@
-{ buildGoModule, ... }:
+{ buildGoModule, go-tools, ... }:
 buildGoModule {
   pname = "ipwatch";
   version = "0.0.2";
   CGO_ENABLED = 0;
   src = ./.;
   vendorSha256 = "sha256-AwH8pC0S4PSYKW6PacqkO8Hfd1YQ9WKnbFFO/zzy2Ow=";
+  preCheck = "HOME=/tmp ${go-tools}/bin/staticcheck ./...";
 }
