76280ab55f29d0c5cc72f57599b8acffc5ec003b
Jared Baur <jaredbaur@fastmail.com>
2022-10-22 10:18:56 -0700

    Version bump

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 { buildGoModule, go-tools, ... }:
 buildGoModule {
   pname = "ipwatch";
-  version = "0.1.5";
+  version = "0.1.6";
   CGO_ENABLED = 0;
   src = ./.;
   vendorSha256 = "sha256-AwH8pC0S4PSYKW6PacqkO8Hfd1YQ9WKnbFFO/zzy2Ow=";
