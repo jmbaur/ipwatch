a7192aaa07082507239e4a950ed1df1d688741ce
Jared Baur <jaredbaur@fastmail.com>
2022-10-23 19:15:17 -0700

    Add a basic test

---
+++ ipwatch/ipwatch_test.go
@@ -0,0 +1,34 @@
+package ipwatch
+
+import (
+	"net/netip"
+	"testing"
+)
+
+func TestPassesFilter(t *testing.T) {
+	tt := []struct {
+		name        string
+		inputAddr   netip.Addr
+		inputFilter string
+		expect      bool
+	}{
+		{
+			name:        "::1 IsLoopback",
+			inputAddr:   netip.MustParseAddr("::1"),
+			inputFilter: "IsLoopback",
+			expect:      true,
+		},
+		{
+			name:        "::1 !IsLoopback",
+			inputAddr:   netip.MustParseAddr("::1"),
+			inputFilter: "!IsLoopback",
+			expect:      false,
+		},
+	}
+
+	for _, tc := range tt {
+		if got := passesFilter(tc.inputAddr, tc.inputFilter); got != tc.expect {
+			t.Fatalf("%s: got: %v, wanted: %v\n", tc.name, got, tc.expect)
+		}
+	}
+}
