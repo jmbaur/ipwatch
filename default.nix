83d33c31b0ea3f54c2e9c635b4bf2212d9014492
Jared Baur <jaredbaur@fastmail.com>
2023-02-23 09:58:55 -0800

    Version bump

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 { buildGoModule, ... }:
 buildGoModule {
   pname = "ipwatch";
-  version = "0.2.2";
+  version = "0.2.3";
   src = ./.;
   vendorSha256 = "sha256-0SGFE9sgYuBWHgITKHR8AwO05R0IKCcpCK0TH3ynmEQ=";
   ldflags = [ "-s" "-w" ];
