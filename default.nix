99a2061f7d130634729b11cf0b685de99dc47d62
jmbaur <45740526+jmbaur@users.noreply.github.com>
2024-03-17 01:15:42 +0000

    Update dependencies

--- default.nix
+++ default.nix
@@ -3,7 +3,7 @@ buildGoModule {
   pname = "ipwatch";
   version = "0.2.3";
   src = ./.;
-  vendorHash = "sha256-w+VTfCBgnbZ8YzYnp2AnQihU8M5OiO/4v0ZVX9UZm8s=";
+  vendorHash = "sha256-qwOSYN5zSsE7LU4eSkAZqCViCZAY1y2NbQjGJQEUYgQ=";
   ldflags = [ "-s" "-w" ];
   CGO_ENABLED = 0;
 }
